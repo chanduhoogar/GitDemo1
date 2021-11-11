347eb40b6a87c26c16eda01c7a0d7e4666c72007
Chandana M <Chandana.M@visionetsystems.com>
2021-11-11 17:45:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 23;
+            if (i > 20)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
